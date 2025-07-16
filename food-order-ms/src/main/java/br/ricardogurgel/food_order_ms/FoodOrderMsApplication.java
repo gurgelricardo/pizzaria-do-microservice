8a122d5a4d5b42dc119c7ca6cf739a0ccac1941a
gurgelricardo <ra24540901-5@aluno.unicesumar.edu.br>
2025-07-15 22:12:38 -0300

    fisrt commmit

---
+++ food-order-ms/src/main/java/br/ricardogurgel/food_order_ms/FoodOrderMsApplication.java
@@ -0,0 +1,13 @@
+package br.ricardogurgel.food_order_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodOrderMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodOrderMsApplication.class, args);
+	}
+
+}
